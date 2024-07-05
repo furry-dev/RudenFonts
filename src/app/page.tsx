44e53d884e6ab1472c13e4efc946880a79f88031
Furry Dev <rudenkomax20072017@gmail.com>
2024-07-05 18:01:29 +0300

    Add revalidate fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 import Home from "@/components/screens/home/Home"
 
 async function fetchData() {
-    const res = await fetch(process.env.API_URL + "/font")
+    const res = await fetch(process.env.API_URL + "/font", {
+        method: "GET",
+        next: {revalidate: 3600}
+    })
+    if (!res.ok) {
+        throw new Error("Failed to fetch data")
+    }
     return await res.json()
 }
 
